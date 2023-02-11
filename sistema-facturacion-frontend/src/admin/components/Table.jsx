3951aa41de0f8a791b238515a0763b10b1e53b32
Kevin Sarria <reysabueso256@gmail.com>
2023-02-11 00:42:51 -0500

    avances

--- sistema-facturacion-frontend/src/admin/components/Table.jsx
+++ sistema-facturacion-frontend/src/admin/components/Table.jsx
@@ -1,19 +1,34 @@
-export const Table = ({ data = [], titulos = [] }) => {
-
+export const Table = ({ data = [] }) => {
   // ejemplo = (<td key={data.id}>{data.id}</td>)
 
- let columns = [];
- let rows = [];
- for ( let index = 0; index < data.length; index++) {
-   rows = [...rows, `<td key=${data[index].id}>${data[index].nombre}</td>`];
-  //  console.log(rows);
-  //  return;
- }
+  let titulos = Object.keys(data[0]);
+  let rows = [];
 
- console.log(rows);
+  for (let index = 0; index < data.length; index++) {
+    rows = [
+      ...rows,
+      <>
+        <tr key={data[index].id}>
+          <td key={`00${index}00`}>{index + 1}</td>
+          <td key={data[index].nombre}>{data[index].nombre}</td>
+          <td key={data[index].cantidad}>{data[index].cantidad}</td>
+          <td key={data[index].valor_medida}>{data[index].valor_medida}</td>
+          <td key={data[index].precio_compra}>{data[index].precio_compra}</td>
+          <td key={data[index].porcentaje_ganancia}>
+            {data[index].porcentaje_ganancia}
+          </td>
+          <td key={data[index].precio_venta}>{data[index].precio_venta}</td>
+        </tr>
+      </>,
+    ];
+  }
+
+  titulos = titulos.map((item) => item.replace("_", " "));
+
+  // console.log(rows);
 
   return (
-    <div className="flex w-5/6 h-1/2 overflow-auto mx-auto">
+    <div className="flex max-w-full h-1/2 overflow-auto mx-auto">
       <table className="w-5/6 mx-auto mt-6 justify-center">
         <thead className="border border-spacing-2 text-center">
           <tr>
@@ -24,15 +39,7 @@ export const Table = ({ data = [], titulos = [] }) => {
         </thead>
 
         <tbody className="border border-spacing-2 text-center">
-          {data.map((dato, index) => {
-            return (
-              <tr key={dato.id}>
-                {
-                  // rows.map( row => row )
-                }
-              </tr>
-            );
-          })}
+          {rows}
         </tbody>
       </table>
     </div>

442c87b4ec1ea67bc693db53c5c8c86fa274fa31
Kevin Sarria <camilo.bravo.2050@gmail.com>
2024-10-04 23:14:10 -0500

    backend base arreglado

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,17 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AuthController;
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::get('/hola', function() {
-    return "hola";
-});
-
-// Registrar usuario
-Route::post('/registro', [AuthController::class, 'register']);
-Route::post('/registro', [AuthController::class, 'register']);
